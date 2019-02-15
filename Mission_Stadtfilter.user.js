3239d8cc9b801647c03421b43b1faab5af7409b8
XFreyaX <43953090+XFreyaX@users.noreply.github.com>
2019-02-15 15:04:58 +0100

    Create Mission_Stadtfilter.user.js

---
+++ Mission_Stadtfilter.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Mission_Stadtfilter
+// @namespace    Leitstellenspiel
+// @version      0.1
+// @description  filtert ungewollte Städte aus der Missionsliste
+// @author       x_Freya_x
+// @include      https://www.leitstellenspiel.de/*
+// @require      https://code.jquery.com/jquery-3.3.1.js
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    var forbidden_places = ["Stadt A",""Stadt B",""Stadt C"]; // Edit this line with city names - this city missions will be hidden
+    var t, tt, i;
+
+    const filterit = () => {
+        var ml = $('.missionSideBarEntry');
+        ml.each(function(){
+            t = $(this);
+            tt = t.text();
+            for (i = 0; i < forbidden_places.length; i++) {
+                if (tt.indexOf(forbidden_places[i]) >= 0) {
+                    console.log(tt);
+                    t.css('display', 'none');
+                } else {
+                }
+            }
+        });
+    };
+
+    let timerID = setInterval(filterit, 20000);
+})();
